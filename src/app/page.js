b84cbba1b52eef8f8d613a01908231817d1682e7
Devyash Saini <dysaini2004@gmail.com>
2024-04-10 00:17:00 +0530

    carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import LandingPage from "./_components/landingPage";
-import PeepShow from "./_components/ui/peepShow";
-import AboutUs from "./_components/AboutUs";
-
+import Test from "./test";
+// import Patri from "./_components/motivation";
 export default function Home() {
   return (
     <div>
-     <LandingPage />
-      <AboutUs /> 
-      {/* <PeepShow /> */}
+       <LandingPage /> 
+      <div>
+        hii
+      </div>
     </div>
     
   );
